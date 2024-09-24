3e698609c9b0a2532e178973630f9e2bcca8f6f3
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-09-24 13:30:36 -0500

    fix: memory build

--- utils/mem_shuffle.py
+++ utils/mem_shuffle.py
@@ -2,25 +2,28 @@ def check_shuffle(x, y, z):
     if len(z) != len(x) + len(y):
         return False
     
-    memo = {}
+    # Create memory with |x| * |y| size
+    dp = [[None] * (len(y) + 1) for _ in range(len(x) + 1)]
     
-    return check_shuffle_aux(x, y, z, 0, 0, 0, memo)
+    return check_shuffle_aux(x, y, z, len(x), len(y), dp)
 
-def check_shuffle_aux(x, y, z, i, j, k, memo):
-    if k == len(z):
-        return i == len(x) and j == len(y)
+def check_shuffle_aux(x, y, z, i, j, dp):
+    if dp[i][j] is not None:
+        return dp[i][j]
     
-    if (i, j, k) in memo:
-        return memo[(i, j, k)]
+    if i == 0 and j == 0:
+        dp[i][j] = True
+        return True
+
+    if i > 0 and z[i + j - 1] == x[i - 1]:
+        if check_shuffle_aux(x, y, z, i - 1, j, dp):
+            dp[i][j] = True
+            return True
     
-    is_valid = False
+    if j > 0 and z[i + j - 1] == y[j - 1]:
+        if check_shuffle_aux(x, y, z, i, j - 1, dp):
+            dp[i][j] = True
+            return True
     
-    if i < len(x) and z[k] == x[i]:
-        is_valid = check_shuffle_aux(x, y, z, i + 1, j, k + 1, memo)
-    
-    if not is_valid and j < len(y) and z[k] == y[j]:
-        is_valid = check_shuffle_aux(x, y, z, i, j + 1, k + 1, memo)
-    
-    memo[(i, j, k)] = is_valid
-    
-    return is_valid
+    dp[i][j] = False
+    return False
