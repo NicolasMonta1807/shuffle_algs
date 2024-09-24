93e82904def2e52688e8382425f09435fecead07
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-09-24 08:34:46 -0500

    test: adding more text examples

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from utils import rec_shuffle, mem_shuffle
 
 def main():
-  x = ['C', 'A', 'S', 'A']
-  y = ['C', 'A', 'R', 'R', 'O']
-  
-  z = ['C', 'C', 'A', 'S', 'A', 'R', 'R', 'A', 'O']
-  
-  rec_res = rec_shuffle.check_shuffle(x, y, z)
-  mem_res = mem_shuffle.check_shuffle(x, y, z)
-  
-  print(f"The shuffle is {'valid' if rec_res else 'invalid'} (Recursive)")
-  print(f"The shuffle is {'valid' if rec_res else 'invalid'} (Memo)")
+  examples = [
+    (['a', 'b'], ['x', 'y'], ['a', 'x', 'b', 'y']),  
+    (['a', 'b'], ['x', 'y'], ['a', 'b', 'x', 'y']),  
+    (['a', 'b'], ['x', 'y'], ['a', 'x', 'y', 'b']),  
+    (['a', 'b'], ['x', 'y'], ['x', 'a', 'b', 'y']),  
+    (['a', 'b'], ['x', 'y'], ['x', 'y', 'a', 'b']),  
+    (['a', 'b'], ['x', 'y'], ['a', 'y', 'b', 'x']),
+  ]
+
+  # Probar cada ejemplo
+  for i, (x, y, z) in enumerate(examples):
+      print(f"\nEjemplo {i+1}: X = {x}, Y = {y}, Z = {z}")
+      
+      rec_res = rec_shuffle.check_shuffle(x, y, z)
+      mem_res = mem_shuffle.check_shuffle(x, y, z)
+      
+      print(f"Recursive result: {rec_res}")
+      print(f"Memoized result: {mem_res}")
 
 if __name__ == "__main__":
   main()
